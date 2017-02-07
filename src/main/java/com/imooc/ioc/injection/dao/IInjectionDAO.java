436b1ed5f60cf42d9388dd97fb0ff1b620b95610
shigl <shigl@192.168.1.105>
2017-02-07 21:30:02 +0800

    全部源码

---
+++ src/main/java/com/imooc/ioc/injection/dao/IInjectionDAO.java
@@ -0,0 +1,5 @@
+package com.imooc.ioc.injection.dao;
+
+public interface IInjectionDAO {
+	public void save(String arg);
+}
---
+++ src/main/java/com/imooc/ioc/injection/service/IInjectionService.java
@@ -0,0 +1,5 @@
+package com.imooc.ioc.injection.service;
+
+public interface IInjectionService {
+	public void save(String arg);
+}
